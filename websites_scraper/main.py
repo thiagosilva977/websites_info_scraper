ae556d7a388f6aaaf5163e2e9d2ccfdb4abb1f35
thiagosilva977 <thiagosilva977@hotmail.com>
2023-03-12 04:01:35 -0300

    add output path parameter and fix docker image

--- websites_scraper/main.py
+++ websites_scraper/main.py
@@ -11,8 +11,9 @@ logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S
 
 
 @click.command("scrape-url")
-@click.option("--url", type=click.STRING, help="", default=None)
-def main(url: str):
+@click.option("--url", type=click.STRING, help="Website url to scrape data", default=None)
+@click.option("--output-path", type=click.STRING, help="Your local path to save files", default="")
+def main(url: str, output_path: str):
     """
     Main program execution.
     https://www.randomlists.com/urls
@@ -21,14 +22,16 @@ def main(url: str):
     docker run -it -v /path/to/local/folder:/data scrapy-image scrapy crawl spider_name -o /data/output.json
 
 
+    :param output_path:
     :type url: url to search
     :return: .json file
     """
     logging.debug(str(f"Initializing data collector"))
-    logging.info(str(f"Searching url {url}"))
+    logging.info(str(f"Searching url: {url}"))
+    logging.info(str(f"Output path: {output_path}"))
 
     try:
-        os.remove('output.json')
+        os.remove('collected_website_data.json')
     except FileNotFoundError:
         pass
 
@@ -45,7 +48,7 @@ def main(url: str):
                                        'RETRY_HTTP_CODES': [500, 502, 503, 504, 522, 524, 408],
                                        'RETRY_TIMES': 5,
                                        'FEED_FORMAT': 'json',
-                                       'FEED_URI': 'output.json',
+                                       'FEED_URI': str(f"{output_path}/collected_website_data.json"),
                                        'SPIDER_MODULES': ['websites_scraper.spiders'],
                                        'TWISTED_REACTOR': 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'}
 
